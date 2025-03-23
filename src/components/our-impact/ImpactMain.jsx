f961c634f3dacfdde35edd95a9dc238f5c729995
Mohit Kumar <mkhicher404@gmail.com>
2025-03-23 19:06:53 +0530

    //

--- src/components/our-impact/ImpactMain.jsx
+++ src/components/our-impact/ImpactMain.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import AnimatedHero from "../common/Components/AnimatedHero";
+import CreatingImpact from "./CreatingImpact";
+import EnvironmentalImpact from "./environmental-impact/EnvironmentalImpact";
+import SocialImpact from "./social-impact/SocialImpact";
+import Recognition from "./recognition/Recognition";
+import Partnerships from "./partnerships/Partnerships";
+import Downloads from "./downloads/Downloads";
+import Blogs from "./blogs/Blogs";
 
 const ImpactMain = () => {
-  return <div>ImpactMain</div>;
+  return (
+    <div>
+      <AnimatedHero />
+      <CreatingImpact />
+      <EnvironmentalImpact />
+      <SocialImpact />
+      <Recognition />
+      <Partnerships />
+      <Downloads />
+      <Blogs />
+    </div>
+  );
 };
 
 export default ImpactMain;
